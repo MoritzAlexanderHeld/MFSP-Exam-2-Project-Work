408c4d8b9cb4e98e7695ee8534a1c934c72b3de3
Moritz Held <moritz.held0027@stud.hkr.se>
2023-03-05 11:04:42 +0100

    Commented print statements out, uncommented actual functions

--- python-template-main/pigdice/menu2.py
+++ python-template-main/pigdice/menu2.py
@@ -17,24 +17,24 @@ class Menu_2:
         print("|----------------|")
 
     def select_option(self, option):
-        # The print statements below are for test purposes only.
+        # The print statements below are there to test on the go.
         if option == "1":
-            print("You selcted Highscore")
-        #    highscore = Highscore()
-        #    highscore.display_highscore()
+            # print("You selcted Highscore")
+            highscore = Highscore()
+            highscore.display_highscore()
         elif option == "2":
-            print("You selected Change Name")
-        #    change_name = ChangeName()
-        #    change_name.change_player_name()
+            # print("You selected Change Name")
+            change_name = ChangeName()
+            change_name.change_player_name()
         elif option == "3":
-            print("You selected Display Rules")
-        #    rules = Rules()
-        #    rules.display_rules()
+            # print("You selected Display Rules")
+            rules = Rules()
+            rules.display_rules()
         elif option == "4":
-            print("You selected Restart and will be brought to the start menu.")
-        #    menu_1 = Menu()
-        #    menu_1.display_menu()
-        #    menu_1.select_option(input("Select an option: "))
+            # print("You selected Restart and will be brought to the start menu.")
+            menu_1 = Menu()
+            menu_1.display_menu()
+            menu_1.select_option(input("Select an option: "))
         elif option == "5":
             exit()
 
