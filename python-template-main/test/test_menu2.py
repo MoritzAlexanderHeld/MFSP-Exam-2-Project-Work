8712b0e9457150d56c54c516b743751774c8db21
MoritzAlexanderHeld <moritz.held0027@stud.hkr.se>
2023-03-05 13:14:53 +0100

    Update test_menu2.py

--- python-template-main/test/test_menu2.py
+++ python-template-main/test/test_menu2.py
@@ -1,3 +1,20 @@
 import unittest
+from unittest.mock import patch
+from pigdice.menu2 import Menu_2
 
-class TestMenu(unittest.TestCase):
+class TestMenu2(unittest.TestCase):
+    @patch("builtins.print")
+    def test_display_menu(self, mock_print):
+        menu = Menu_2()
+        menu.print_menu()
+        expected_output = """*----------------*
+|      MENU      |
+|----------------|
+| 1. Highscore   |
+| 2. Change name |
+| 3. View rules  |
+| 4. Restart     |
+| 5. Exit        |
+|----------------|
+"""
+        mock_print.assert_called_once_with(expected_output)
