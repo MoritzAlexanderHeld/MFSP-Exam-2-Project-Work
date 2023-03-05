333bc764d083d0aac50531529719ff4532c2b28e
MoritzAlexanderHeld <moritz.held0027@stud.hkr.se>
2023-03-05 17:53:42 +0100

    Changed to assertEqual, doesnt wor
    Changed the test to use "assertEqual" instead of mocking the print, but that leads to the problem that a string (expected_output) is compared to an object. Need to find a way to put the generated output of the object in the () or find a whole different way again.

--- python-template-main/test/test_menu2.py
+++ python-template-main/test/test_menu2.py
@@ -1,12 +1,10 @@
 import unittest
-from unittest.mock import patch
 from pigdice.menu2 import Menu_2
 
 class TestMenu2(unittest.TestCase):
-    @patch("builtins.print")
-    def test_display_menu(self, mock_print):
-        menu = Menu_2()
-        menu.print_menu()
+    def test_display_menu(self):
+        menu2 = Menu_2()
+        menu2.print_menu()
         expected_output = """*----------------*
 |      MENU      |
 |----------------|
@@ -15,6 +13,9 @@ class TestMenu2(unittest.TestCase):
 | 3. View rules  |
 | 4. Restart     |
 | 5. Exit        |
-|----------------|
-"""
-        mock_print.assert_called_once_with(expected_output)
+|----------------|"""
+
+# The current problem is that a string (expected_output is compared to an object.
+# To use this method I have to pass the string that the object prints instead.
+# Simply doing str(menu2) does not work, neither does "menu2.print_menu()"
+        self.assertEqual(expected_output, menu2.print_menu())
