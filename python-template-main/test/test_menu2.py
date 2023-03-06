fa0b7296bc1a3a937a52cd4c29d31c31bc3bcbe7
MoritzAlexanderHeld <moritz.held0027@stud.hkr.se>
2023-03-06 10:31:41 +0100

    Update test_menu2.py
    Printed output redirected to the buffer to take it from there as string and compare it to the expected output.
    Still doesn't work, but now it seems to be an issue with the format of the menu, not with the type.

--- python-template-main/test/test_menu2.py
+++ python-template-main/test/test_menu2.py
@@ -1,10 +1,14 @@
 import unittest
 from pigdice.menu2 import Menu_2
+import io
+from contextlib import redirect_stdout
 
 class TestMenu2(unittest.TestCase):
     def test_display_menu(self):
         menu2 = Menu_2()
-        menu2.print_menu()
+        with io.StringIO() as buf, redirect_stdout(buf):
+            menu2.print_menu()
+            printed_output = buf.getvalue()
         expected_output = """*----------------*
 |      MENU      |
 |----------------|
@@ -18,4 +22,4 @@ class TestMenu2(unittest.TestCase):
 # The current problem is that a string (expected_output is compared to an object.
 # To use this method I have to pass the string that the object prints instead.
 # Simply doing str(menu2) does not work, neither does "menu2.print_menu()"
-        self.assertEqual(expected_output, menu2.print_menu())
+        self.assertEqual(expected_output, printed_output)
